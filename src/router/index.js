12b3ed236da54ab2906bcdc73a6769018e841eeb
Jo1nson <626837428@qq.com>
2020-11-05 20:32:59 +0800

    完成了报表功能的开发

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,7 @@ import Params from '../components/goods/Params'
 import GoodList from '../components/goods/List'
 import Add from '../components/goods/Add'
 import Order from '../components/order/Order'
+import Report from '../components/report/Report'
 
 Vue.use(VueRouter)
 
@@ -31,6 +32,7 @@ const router = new VueRouter({
         {path:'/goods',component:GoodList},
         {path:'/goods/add',component:Add},
         {path:'/orders',component:Order},
+        {path:'/reports',component:Report},
       ] }
   ]
 })
